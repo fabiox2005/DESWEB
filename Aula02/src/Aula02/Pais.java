c4e6f563310d3b7618b65b14c2cd496e0a43f912
fabio-sotnas <fabio-sotnas@fabio-sotnas-PC>
2019-03-17 17:48:54 -0300

    Aula02

---
+++ Aula02/src/Aula02/Pais.java
@@ -0,0 +1,231 @@
+package Aula02;
+
+/*
+b. Crie uma classe Pais com os atributos id (int), nome (String), populacao (long) e area
+(double).
+c. Crie um construtor com os campos, um construtor padr�o, gets e sets.
+d. Crie os m�todos do CRUD de Pais.
+e. Crie um m�todo que retorna o pa�s com maior n�mero de habitantes.
+f. Crie um m�todo que retorna o pa�s com menor �rea.
+g. Crie um m�todo que retorne um vetor de 3 pa�ses.
+h. Crie uma classe de testes que, al�m de testar os CRUDs, teste tamb�m os m�todos
+maiorPopulacao, menorArea e vetorTresPaises.
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Pais {
+
+	private int id;
+	private String nome;
+	private long populacao;
+	private double area;
+
+	static {
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	public Pais() {
+
+	}
+
+	public Pais(int id, String nome, long populacao, double area) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.populacao = populacao;
+		this.area = area;
+	}
+
+	// Obt�m conex�o com o banco de dados
+	public Connection obtemConexao() throws SQLException {
+		return DriverManager.getConnection("jdbc:mysql://localhost/Paises?user=alunos&password=alunos");
+	}
+
+	public void criar() {
+		String sqlInsert = "INSERT INTO pais(nome, populacao, area) VALUES (?, ?, ?)";
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = obtemConexao(); PreparedStatement stm = conn.prepareStatement(sqlInsert);) {
+			stm.setString(1, getNome());
+			stm.setLong(2, getPopulacao());
+			stm.setDouble(3, getArea());
+			stm.execute();
+			String sqlQuery = "SELECT LAST_INSERT_ID()";
+			try (PreparedStatement stm2 = conn.prepareStatement(sqlQuery); ResultSet rs = stm2.executeQuery();) {
+				if (rs.next()) {
+					setId(rs.getInt(1));
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void atualizar() {
+		String sqlUpdate = "UPDATE pais SET nome=?, populacao=?, area=? WHERE id=?";
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = obtemConexao(); PreparedStatement stm = conn.prepareStatement(sqlUpdate);) {
+			stm.setString(1, getNome());
+			stm.setLong(2, getPopulacao());
+			stm.setDouble(3, getArea());
+			stm.setInt(4, getId());
+			stm.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void excluir() {
+		String sqlDelete = "DELETE FROM pais WHERE id = ?";
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = obtemConexao(); PreparedStatement stm = conn.prepareStatement(sqlDelete);) {
+			stm.setInt(1, getId());
+			stm.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void carregar() {
+		String sqlSelect = "SELECT nome, populacao, area FROM pais WHERE pais.id = ?";
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = obtemConexao(); PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
+			stm.setInt(1, getId());
+			try (ResultSet rs = stm.executeQuery();) {
+				if (rs.next()) {
+					setNome(rs.getString("nome"));
+					setPopulacao(rs.getLong("populacao"));
+					setArea(rs.getDouble("area"));
+				} else {
+					setId(-1);
+					setNome(null);
+					setPopulacao(-1);
+					setArea(-1);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e1) {
+			System.out.print(e1.getStackTrace());
+		}
+	}
+
+	// ---------------------------------------------------------//
+
+	public String maiorPopulacao() {
+		String sqlSelect = "SELECT * FROM pais WHERE populacao = (SELECT MAX(populacao) FROM pais)";
+		try (Connection conn = obtemConexao(); PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
+			stm.setInt(1, getId());
+			try (ResultSet rs = stm.executeQuery();) {
+				if (rs.next()) {
+					setNome(rs.getString("nome"));
+					setPopulacao(rs.getLong("populacao"));
+
+				} else {
+					setId(-1);
+					setNome(null);
+					setPopulacao(-1);
+
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e1) {
+			System.out.print(e1.getStackTrace());
+		}
+		return sqlSelect;
+	}
+
+	public String menorArea() {
+		String sqlSelect = "SELECT * FROM pais WHERE area = (SELECT MIN(area) FROM pais)";
+		try (Connection conn = obtemConexao(); PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
+			stm.setInt(1, getId());
+			try (ResultSet rs = stm.executeQuery();) {
+				if (rs.next()) {
+					setNome(rs.getString("nome"));
+					setArea(rs.getDouble("area"));
+
+				} else {
+					setId(-1);
+					setNome(null);
+					setArea(-1);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e1) {
+			System.out.print(e1.getStackTrace());
+		}
+		return sqlSelect;
+	}
+
+	public String VetorTresPaises() {
+		String sqlSelect = "SELECT * FROM pais ORDER BY RAND() Limit 3";
+		try (Connection conn = obtemConexao(); PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
+			stm.setInt(1, getId());
+			try (ResultSet rs = stm.executeQuery();) {
+				if (rs.next()) {
+					setNome(rs.getString("nome"));
+					setPopulacao(rs.getLong("populacao"));
+					setArea(rs.getDouble("area"));
+				} else {
+					setId(-1);
+					setNome(null);
+					setPopulacao(-1);
+					setArea(-1);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e1) {
+			System.out.print(e1.getStackTrace());
+		}
+		return sqlSelect;
+	}
+
+	// ---------------------------------------------------------//
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public long getPopulacao() {
+		return populacao;
+	}
+
+	public void setPopulacao(long populacao) {
+		this.populacao = populacao;
+	}
+
+	public double getArea() {
+		return area;
+	}
+
+	public void setArea(double area) {
+		this.area = area;
+	}
+
+}
